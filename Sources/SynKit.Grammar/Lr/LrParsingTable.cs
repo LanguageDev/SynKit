bbd996c3ae0c3483af789b1178ba7e3fb9f3db83
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-11-13 11:15:45 +0100

    Added SLR table construction

--- Sources/SynKit.Grammar/Lr/LrParsingTable.cs
+++ Sources/SynKit.Grammar/Lr/LrParsingTable.cs
@@ -76,6 +76,76 @@ public static class LrParsingTable
         return new(grammar, stateAllocator, actionTable, gotoTable);
     }
 
+    /// <summary>
+    /// Builds an SLR parsing table.
+    /// </summary>
+    /// <param name="grammar">The grammar to build the table for.</param>
+    /// <returns>The SLR table for <paramref name="grammar"/>.</returns>
+    public static LrParsingTable<Lr0Item> Slr(ContextFreeGrammar grammar)
+    {
+        if (grammar.StartSymbol is null) throw new InvalidOperationException("The grammar must have a start symbol!");
+
+        var startProductions = grammar.GetProductions(grammar.StartSymbol);
+
+        var stateAllocator = new LrStateAllocator<Lr0Item>();
+        var actionTable = new LrActionTable();
+        var gotoTable = new LrGotoTable();
+
+        // Construct the I0 set
+        var i0 = Lr0Closure(grammar, startProductions.Select(p => new Lr0Item(p, 0)));
+        var stk = new Stack<(LrItemSet<Lr0Item> ItemSet, LrState State)>();
+        stateAllocator.Allocate(i0, out var state0);
+        stk.Push((i0, state0));
+
+        while (stk.TryPop(out var itemSetPair))
+        {
+            var itemSet = itemSetPair.ItemSet;
+            var state = itemSetPair.State;
+
+            // Terminal advance
+            var itemsWithTerminals = itemSet.Items
+                .Where(prod => prod.AfterCursor is Symbol.Terminal)
+                .GroupBy(prod => prod.AfterCursor);
+            foreach (var group in itemsWithTerminals)
+            {
+                var term = (Symbol.Terminal)group.Key!;
+                var nextSet = Lr0Closure(grammar, group.Select(i => i.Next));
+                if (stateAllocator.Allocate(nextSet, out var nextState)) stk.Push((nextSet, nextState));
+                actionTable[state, term].Add(new LrAction.Shift(nextState));
+            }
+
+            // Nonterminal advance
+            var itemsWithNonterminals = itemSet.Items
+                .Where(prod => prod.AfterCursor is Symbol.Nonterminal)
+                .GroupBy(prod => prod.AfterCursor);
+            foreach (var group in itemsWithNonterminals)
+            {
+                var nonterm = (Symbol.Nonterminal)group.Key!;
+                var nextSet = Lr0Closure(grammar, group.Select(i => i.Next));
+                if (stateAllocator.Allocate(nextSet, out var nextState)) stk.Push((nextSet, nextState));
+                gotoTable[state, nonterm] = nextState;
+            }
+
+            // Final items
+            var finalItems = itemSet.Items.Where(prod => prod.IsFinal);
+            foreach (var finalItem in finalItems)
+            {
+                if (finalItem.Production.Left.Equals(grammar.StartSymbol))
+                {
+                    actionTable[state, Symbol.Terminal.EndOfInput].Add(LrAction.Accept.Instance);
+                }
+                else
+                {
+                    var reduction = new LrAction.Reduce(finalItem.Production);
+                    var followSet = grammar.FollowSet(finalItem.Production.Left);
+                    foreach (var follow in followSet) actionTable[state, follow].Add(reduction);
+                }
+            }
+        }
+
+        return new(grammar, stateAllocator, actionTable, gotoTable);
+    }
+
     private static LrItemSet<Lr0Item> Lr0Closure(
         ContextFreeGrammar grammar,
         IEnumerable<Lr0Item> set) => Closure(grammar, set, (item, prod) => new[] { new Lr0Item(prod, 0) });
