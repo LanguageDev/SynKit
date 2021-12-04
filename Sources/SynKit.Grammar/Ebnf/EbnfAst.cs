2a0b3e02cf8428604a1a2a58609d124945359379
Lenkefi Péter <lenkefi.peti@gmail.com>
2021-12-04 23:10:13 +0100

    Rewrote EBNF conversion

--- Sources/SynKit.Grammar/Ebnf/EbnfAst.cs
+++ Sources/SynKit.Grammar/Ebnf/EbnfAst.cs
@@ -6,53 +6,146 @@ namespace SynKit.Grammar.Ebnf;
 public abstract record EbnfAst
 {
     /// <summary>
+    /// Creates a node that represents an optional element.
+    /// </summary>
+    /// <param name="element">The element to make optional.</param>
+    /// <returns>A node that represents matching an optional <paramref name="element"/>.</returns>
+    public static EbnfAst Opt(EbnfAst element) => new Alt(element, Epsilon.Instance);
+
+    /// <summary>
+    /// Creates a repetition node that represents 0 or more repetitions.
+    /// </summary>
+    /// <param name="element">The element to repeat.</param>
+    /// <returns>A repetition node that represents matching <paramref name="element"/> 0 or more times.</returns>
+    public static EbnfAst ZeroOrMore(EbnfAst element) => new Rep(element, 0, null);
+
+    /// <summary>
+    /// Creates a repetition node that represents 1 or more repetitions.
+    /// </summary>
+    /// <param name="element">The element to repeat.</param>
+    /// <returns>A repetition node that represents matching <paramref name="element"/> 1 or more times.</returns>
+    public static EbnfAst OneOrMore(EbnfAst element) => new Rep(element, 1, null);
+
+    /// <summary>
+    /// Creates a repetition node that represents at least a given amount of repetitions.
+    /// </summary>
+    /// <param name="element">The element to repeat.</param>
+    /// <param name="n">The minimum number of repetitions.</param>
+    /// <returns>A repetition node that represents matching <paramref name="element"/> <paramref name="n"/>
+    /// or more times.</returns>
+    public static EbnfAst AtLeast(EbnfAst element, int n) => new Rep(element, n, null);
+
+    /// <summary>
+    /// Creates a repetition node that represents at most a given amount of repetitions.
+    /// </summary>
+    /// <param name="element">The element to repeat.</param>
+    /// <param name="n">The maximum number of repetitions.</param>
+    /// <returns>A repetition node that represents matching <paramref name="element"/> <paramref name="n"/>
+    /// or less times.</returns>
+    public static EbnfAst AtMost(EbnfAst element, int n) => new Rep(element, 0, n);
+
+    /// <summary>
+    /// Creates a repetition node that represents repetition in a given range.
+    /// </summary>
+    /// <param name="element">The element to repeat.</param>
+    /// <param name="min">The minimum number of repetitions.</param>
+    /// <param name="max">The maximum number of repetitions.</param>
+    /// <returns>A repetition node that represents matching <paramref name="element"/> between <paramref name="min"/>
+    /// (inclusive) and <paramref name="max"/> (inclusive) times.</returns>
+    public static Rep Between(EbnfAst element, int min, int max) => new(element, min, max);
+
+    /// <summary>
+    /// Normalizes this node so it contains alternatives on top, sequences below that, and only has 0-or-more
+    /// unbounded repetitions.
+    /// </summary>
+    /// <returns>The normalized, equivalent eBNF tree.</returns>
+    public abstract EbnfAst Normalize();
+
+    /// <summary>
+    /// Represents a node that always matches an empty word.
+    /// </summary>
+    public sealed record Epsilon : EbnfAst
+    {
+        /// <summary>
+        /// The instance to use.
+        /// </summary>
+        public static Epsilon Instance { get; } = new();
+
+        private Epsilon()
+        {
+        }
+
+        /// <inheritdoc/>
+        public override EbnfAst Normalize() => this;
+    }
+
+    /// <summary>
     /// Represents an eBNF rule reference.
     /// </summary>
     /// <param name="Name">The rule name.</param>
-    public sealed record Rule(string Name) : EbnfAst;
+    public sealed record Rule(string Name) : EbnfAst
+    {
+        /// <inheritdoc/>
+        public override EbnfAst Normalize() => this;
+    }
 
     /// <summary>
     /// Represents a terminal value.
     /// </summary>
     /// <param name="Value">The object identifying the terminal.</param>
-    public sealed record Term(object Value) : EbnfAst;
+    public sealed record Term(object Value) : EbnfAst
+    {
+        /// <inheritdoc/>
+        public override EbnfAst Normalize() => this;
+    }
 
     /// <summary>
     /// Represents an eBNF node of two alternative constructs.
     /// </summary>
     /// <param name="Left">The first alternative.</param>
     /// <param name="Right">The second alternative.</param>
-    public sealed record Alt(EbnfAst Left, EbnfAst Right) : EbnfAst;
+    public sealed record Alt(EbnfAst Left, EbnfAst Right) : EbnfAst
+    {
+        /// <inheritdoc/>
+        public override EbnfAst Normalize() => new Alt(this.Left.Normalize(), this.Right.Normalize());
+    }
 
     /// <summary>
     /// Represents a sequence - or concatenation - of two eBNF nodes.
     /// </summary>
     /// <param name="Left">The first node.</param>
     /// <param name="Right">The second node.</param>
-    public sealed record Seq(EbnfAst Left, EbnfAst Right) : EbnfAst;
-
-    /// <summary>
-    /// Represents an optional version of another eBNF construct.
-    /// </summary>
-    /// <param name="Element">The construct to make optional.</param>
-    public sealed record Opt(EbnfAst Element) : EbnfAst;
-
-    /// <summary>
-    /// Represents a 0-or-more repetition of another eBNF construct.
-    /// </summary>
-    /// <param name="Element">The construct to repeat.</param>
-    public sealed record Rep0(EbnfAst Element) : EbnfAst;
-
-    /// <summary>
-    /// Represents a 1-or-more repetition of another eBNF construct.
-    /// </summary>
-    /// <param name="Element">The construct to repeat.</param>
-    public sealed record Rep1(EbnfAst Element) : EbnfAst;
+    public sealed record Seq(EbnfAst Left, EbnfAst Right) : EbnfAst
+    {
+        /// <inheritdoc/>
+        public override EbnfAst Normalize()
+        {
+            var leftNorm = this.Left.Normalize();
+            var rightNorm = this.Right.Normalize();
+            if (leftNorm is Epsilon) return rightNorm;
+            if (rightNorm is Epsilon) return leftNorm;
+            if (leftNorm is Alt leftAlt)
+            {
+                // (X | Y) Z => X Z | Y Z
+                return new Alt(
+                    new Seq(leftAlt.Left, rightNorm).Normalize(),
+                    new Seq(leftAlt.Right, rightNorm).Normalize());
+            }
+            if (rightNorm is Alt rightAlt)
+            {
+                // X (Y | Z) => X Y | X Z
+                return new Alt(
+                    new Seq(leftNorm, rightAlt.Left).Normalize(),
+                    new Seq(leftNorm, rightAlt.Right).Normalize());
+            }
+            return new Seq(leftNorm, rightNorm);
+        }
+    }
 
     /// <summary>
     /// Represents a repetition between an interval of times.
     /// </summary>
-    public sealed record RepN : EbnfAst
+    public sealed record Rep : EbnfAst
     {
         /// <summary>
         /// The construct to repeat.
@@ -70,12 +163,12 @@ public abstract record EbnfAst
         public int? Max { get; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="RepN"/> class.
+        /// Initializes a new instance of the <see cref="Rep"/> class.
         /// </summary>
         /// <param name="element">The construct to repeat.</param>
         /// <param name="min">The minimum amount of repetitions.</param>
         /// <param name="max">The maximum amount of repetitions. Null, if unbounded.</param>
-        public RepN(EbnfAst element, int min, int? max)
+        public Rep(EbnfAst element, int min, int? max)
         {
             if (min < 0) throw new ArgumentOutOfRangeException(nameof(min), "Min can't be less than 0.");
             if (max is not null && max.Value < min) throw new ArgumentOutOfRangeException(nameof(max), "Max can't be smaller than min.");
@@ -83,6 +176,30 @@ public abstract record EbnfAst
             this.Element = element;
             this.Min = min;
             this.Max = max;
+        }
+
+        /// <inheritdoc/>
+        public override EbnfAst Normalize()
+        {
+            var elementNorm = this.Element.Normalize();
+            // Construct the minimum required
+            EbnfAst result = Epsilon.Instance;
+            if (this.Min > 0)
+            {
+                result = elementNorm;
+                for (var i = 1; i < this.Min; ++i) result = new Seq(result, elementNorm);
+            }
+
+            // No upper bound, just add a 0-or-more at the end
+            if (this.Max is null) return new Seq(result, new Rep(elementNorm, 0, null)).Normalize();
+
+            // There's an upper bound, need to append optional elements
+            // We solve that by generating a sequence of (Element | Epsilon)
+            var opt = new Alt(elementNorm, Epsilon.Instance);
+            for (var i = this.Min + 1; i <= this.Max.Value; ++i) result = new Seq(result, opt);
+
+            // We are done
+            return result.Normalize();
         }
     }
 }
--- Sources/SynKit.Grammar/Ebnf/Internal/EbnfToCfConverter.cs
+++ Sources/SynKit.Grammar/Ebnf/Internal/EbnfToCfConverter.cs
@@ -1,4 +1,5 @@
 using SynKit.Grammar.ContextFree;
+using System.Diagnostics;
 
 namespace SynKit.Grammar.Ebnf.Internal;
 
@@ -9,6 +10,7 @@ internal sealed class EbnfToCfConverter
 
     private readonly EbnfGrammar ebnfGrammar;
     private readonly CfGrammar cfg = new();
+    private readonly HashSet<string> validRules = new();
     private int subruleCounter = 0;
 
     public EbnfToCfConverter(EbnfGrammar ebnfGrammar)
@@ -19,6 +21,8 @@ internal sealed class EbnfToCfConverter
     public CfGrammar ToCf()
     {
         if (this.ebnfGrammar.StartRule is null) throw new InvalidOperationException("The starting rule of the eBNF grammar is not set.");
+
+        this.RegisterAllRules();
         this.CheckRulePresence(this.ebnfGrammar.StartRule);
 
         this.cfg.AddProduction(new(Symbol.Nonterminal.Start, new Symbol[] { new Symbol.Nonterminal(this.ebnfGrammar.StartRule) }));
@@ -28,135 +32,60 @@ internal sealed class EbnfToCfConverter
 
     private void ConvertRule(string name, EbnfAst ast)
     {
+        ast = ast.Normalize();
         var left = new Symbol.Nonterminal(name);
-        foreach (var rightSeq in this.ConvertNode(name, ast))
+        foreach (var rightSeq in this.ConvertAltLevel(name, ast))
         {
             var right = rightSeq.ToList();
             this.cfg.AddProduction(new(left, right));
         }
     }
 
-    private IEnumerable<IEnumerable<Symbol>> ConvertNode(string name, EbnfAst node) => node switch
+    private IEnumerable<IEnumerable<Symbol>> ConvertAltLevel(string name, EbnfAst node)
     {
-        EbnfAst.Rule rule => this.ConvertRuleNode(rule),
-        EbnfAst.Term term => ConvertTermNode(term),
-        EbnfAst.Alt alt => this.ConvertAltNode(name, alt),
-        EbnfAst.Seq seq => this.ConvertSeqNode(name, seq),
-        EbnfAst.Opt opt => this.ConvertOptNode(name, opt),
-        EbnfAst.Rep0 rep0 => this.ConvertRep0Node(name, rep0),
-        EbnfAst.Rep1 rep1 => this.ConvertRep1Node(name, rep1),
-        EbnfAst.RepN repn => this.ConvertRepNNode(name, repn),
-        _ => throw new ArgumentOutOfRangeException(nameof(node)),
-    };
-
-    private IEnumerable<IEnumerable<Symbol>> ConvertRuleNode(EbnfAst.Rule rule)
-    {
-        this.CheckRulePresence(rule.Name);
-        yield return new[] { new Symbol.Nonterminal(rule.Name) };
+        if (node is not EbnfAst.Alt alt) return new[] { this.ConvertSeqLevel(name, node) };
+        return this.ConvertAltLevel(name, alt.Left).Concat(this.ConvertAltLevel(name, alt.Right));
     }
 
-    private static IEnumerable<IEnumerable<Symbol>> ConvertTermNode(EbnfAst.Term term)
+    private IEnumerable<Symbol> ConvertSeqLevel(string name, EbnfAst node)
     {
-        yield return new[] { new Symbol.Terminal(term.Value) };
+        if (node is not EbnfAst.Seq seq) return this.ConvertAtomLevel(name, node);
+        return this.ConvertSeqLevel(name, seq.Left).Concat(this.ConvertSeqLevel(name, seq.Right));
     }
 
-    private IEnumerable<IEnumerable<Symbol>> ConvertAltNode(string name, EbnfAst.Alt alt) =>
-        this.ConvertNode(name, alt.Left).Concat(this.ConvertNode(name, alt.Right));
-
-    private IEnumerable<IEnumerable<Symbol>> ConvertSeqNode(string name, EbnfAst.Seq seq)
+    private IEnumerable<Symbol> ConvertAtomLevel(string name, EbnfAst node)
     {
-        var lefts = this.ConvertNode(name, seq.Left).ToList();
-        var rights = this.ConvertNode(name, seq.Right).ToList();
-        foreach (var l in lefts)
+        if (node is EbnfAst.Epsilon) return Enumerable.Empty<Symbol>();
+        if (node is EbnfAst.Term term) return new[] { new Symbol.Terminal(term.Value) };
+        if (node is EbnfAst.Rule rule)
         {
-            foreach (var r in rights) yield return l.Concat(r);
+            this.CheckRulePresence(rule.Name);
+            return new[] { new Symbol.Nonterminal(rule.Name) };
         }
-    }
-
-    private IEnumerable<IEnumerable<Symbol>> ConvertOptNode(string name, EbnfAst.Opt opt)
-    {
-        foreach (var item in this.ConvertNode(name, opt.Element)) yield return item;
-        yield return Array.Empty<Symbol>();
-    }
-
-    private IEnumerable<IEnumerable<Symbol>> ConvertRep0Node(string name, EbnfAst.Rep0 rep0)
-    {
-        var symbol = this.ConvertRepAsSubrule(name, new(rep0.Element, 0, null));
-        yield return new[] { symbol };
-    }
-
-    private IEnumerable<IEnumerable<Symbol>> ConvertRep1Node(string name, EbnfAst.Rep1 rep1)
-    {
-        var symbol = this.ConvertRepAsSubrule(name, new(rep1.Element, 1, null));
-        yield return new[] { symbol };
-    }
-
-    private IEnumerable<IEnumerable<Symbol>> ConvertRepNNode(string name, EbnfAst.RepN repn)
-    {
-        var symbol = this.ConvertRepAsSubrule(name, repn);
-        yield return new[] { symbol };
-    }
-
-    private Symbol ConvertRepAsSubrule(string name, EbnfAst.RepN node)
-    {
-        var subName = $"{name}@sub{this.subruleCounter++}";
-        var subSym = new Symbol.Nonterminal(subName);
-        // Construct min prefix
-        var minSymbols = (IEnumerable<IEnumerable<Symbol>>)new[] { Enumerable.Empty<Symbol>() };
-        if (node.Min != 0)
+        if (node is EbnfAst.Rep rep)
         {
-            var preNode = node.Element;
-            for (var i = 1; i < node.Min; ++i) preNode = new EbnfAst.Seq(preNode, node.Element);
-            minSymbols = this.ConvertNode(subName, preNode);
+            Debug.Assert(rep.Min == 0);
+            Debug.Assert(rep.Max is null);
+            // Make a sub-rule in the form
+            // Sub -> Sub Element | Epsilon
+            // or
+            // Sub -> Element Sub | Epsilon
+            // Depending on which recursion we prefer
+            var sub = this.MakeSubruleName(name);
+            this.validRules.Add(sub);
+            var recAst = PreferLeftRecursion
+                ? new EbnfAst.Seq(new EbnfAst.Rule(sub), rep.Element)
+                : new EbnfAst.Seq(rep.Element, new EbnfAst.Rule(sub));
+            this.ConvertRule(sub, new EbnfAst.Alt(recAst, EbnfAst.Epsilon.Instance));
         }
-        if (node.Max is null)
-        {
-            // No upper-bound, recursive
-            foreach (var minSymbol in minSymbols.Select(s => s.ToList()))
-            {
-                // Add 0 case
-                this.cfg.AddProduction(new(subSym, minSymbol));
-                // Add 1 and 1+ cases
-                foreach (var repSymbol in this.ConvertNode(name, node.Element).Select(s => s.ToList()))
-                {
-                    // 1 case
-                    var right = minSymbol.Concat(repSymbol).ToList();
-                    this.cfg.AddProduction(new(subSym, right));
-                    // 1+ case
-                    var right1 = minSymbol
-                        .Concat(PreferLeftRecursion ? repSymbol.Prepend(subSym) : repSymbol.Append(subSym))
-                        .ToList();
-                    this.cfg.AddProduction(new(subSym, right1));
-                }
-            }
-        }
-        else
-        {
-            // Upper-bound, nonrecursive
-            // Add exactly min case
-            foreach (var minSymbol in minSymbols.Select(s => s.ToList())) this.cfg.AddProduction(new(subSym, minSymbol));
-            // Add min + i, i > 0 cases by constructing a sequence of optionals
-            EbnfAst? optRight = null;
-            for (var i = node.Min + 1; i <= node.Max.Value; ++i)
-            {
-                var part = new EbnfAst.Opt(node.Element);
-                optRight = optRight == null ? part : new EbnfAst.Seq(optRight, part);
-            }
-            // If there's at least 1 optional, append those
-            if (optRight is not null)
-            {
-                var rightSymbols = this.ConvertNode(subName, optRight);
-                foreach (var minSymbol in minSymbols.Select(s => s.ToList()))
-                {
-                    foreach (var rightSymbol in rightSymbols)
-                    {
-                        var seq = minSymbol.Concat(rightSymbol).ToList();
-                        this.cfg.AddProduction(new(subSym, seq));
-                    }
-                }
-            }
-        }
-        return subSym;
+        throw new ArgumentOutOfRangeException(nameof(node));
+    }
+
+    private string MakeSubruleName(string name) => $"{name}@sub{this.subruleCounter++}";
+
+    private void RegisterAllRules()
+    {
+        foreach (var rule in this.ebnfGrammar.Rules.Keys) this.validRules.Add(rule);
     }
 
     private void CheckRulePresence(string name)
